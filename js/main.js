69ed59d23edbb7a9ee7e5264bd1299d52e56e505
kuo20210808 <88610974+kuo20210808@users.noreply.github.com>
2023-07-05 16:21:47 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 $(function () {
-
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        alert('1')
+    }
     setTimeout(function () {
         $('body').removeClass('loading')
         ScrollCenter()
